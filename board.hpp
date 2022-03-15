d7d32f9156e2005cdbeb73813fcc7460bc8bd858
wermos <63574588+wermos@users.noreply.github.com>
2022-03-15 22:54:05 +0530

    Defined the tentative API of the `Board` class.

---
+++ board.hpp
@@ -0,0 +1,34 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+class Board {
+	public:
+		Board() : m_board({{None, None, None}, {None, None, None}, {None, None, None}}) {}
+
+		// Board(const Board& b) : m_board(b.m_board) {}
+
+		enum State : std::uint8_t {
+			XWin,
+			OWin,
+			Draw,
+			None,
+		};
+
+		enum Move : std::uint8_t {
+			None,
+			X,
+			O,
+		};
+
+		State checkWin() const;
+
+		void setCell(std::size_t index, const Move m);
+
+		void printBoard() const;
+
+	private:
+		Move m_board[3][3];
+};
+
+#endif //BOARD_HPP
+
